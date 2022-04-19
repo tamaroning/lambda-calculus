2a97a696311f5b21810e9e4ad186ea682a11d1ce
tamaron <tamaron1203@gmail.com>
2022-04-19 10:59:04 +0900

    add tests

--- typed-lambda/src/main.rs
+++ typed-lambda/src/main.rs
@@ -1,7 +1,7 @@
 use chumsky::prelude::*;
 use chumsky::Parser;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 enum Term {
     Lit(Lit),
     Var(Var),
@@ -20,7 +20,7 @@ impl Var {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 enum Lit {
     Unit,
     True,
@@ -39,8 +39,8 @@ enum Type {
 fn parser() -> impl Parser<char, Term, Error = Simple<char>> {
     let lambda = just("lambda").padded();
     let dot = just(".").padded();
-    let open_paren = just("(").padded();
-    let close_paren = just(")").padded();
+    let open_paren = just("(").padded().debug("(");
+    let close_paren = just(")").padded().debug(")");
     let colon = just(":").padded();
     let arrow = just("->").padded();
 
@@ -49,7 +49,8 @@ fn parser() -> impl Parser<char, Term, Error = Simple<char>> {
     let bool = just("Bool").padded().to(Type::Bool);
     let base_ty = choice((unit, bool));
     let ty = recursive(|ty| {
-        let fnty = base_ty.clone()
+        let fnty = base_ty
+            .clone()
             .clone()
             .then_ignore(arrow)
             .then(ty)
@@ -59,42 +60,53 @@ fn parser() -> impl Parser<char, Term, Error = Simple<char>> {
     });
 
     // Type Annotation
-    let ty_annot = colon.ignore_then(ty);
+    let ty_annot = colon.ignore_then(ty).debug("ty_annot");
 
     // Literal
     let lit = choice((
-        just("unit").padded().to(Lit::Unit),
-        just("true").padded().to(Lit::True),
-        just("false").padded().to(Lit::False),
-    ));
+        just("unit").padded().to(Lit::Unit).debug("unit"),
+        just("true").padded().to(Lit::True).debug("true"),
+        just("false").padded().to(Lit::False).debug("false"),
+    ))
+    .debug("lit");
 
     // Identifier
-    let ident = text::ident::<char, Simple<char>>().padded().map(Var::new);
+    let ident = text::ident::<char, Simple<char>>()
+        .padded()
+        .map(Var::new)
+        .debug("ident");
 
     // Atomic
-    let atom = choice((lit.map(Term::Lit), ident.map(Term::Var)));
+    let atom = choice((lit.map(Term::Lit), ident.clone().map(Term::Var)));
 
     recursive(|term| {
-        let paren_term = open_paren
-            .ignore_then(term.clone())
-            .then_ignore(close_paren);
+        let paren_term = term
+            .clone()
+            .delimited_by(open_paren, close_paren)
+            .debug("paren_term");
 
         let smallest = choice((atom, paren_term));
 
-        let app = 
-            smallest // abs and app don't come first
+        let app = smallest // abs and app don't come first
             .clone()
             .then(term.clone())
-            .map(|(l, r)| Term::App(Box::new(l), Box::new(r)));
+            .map(|(l, r)| Term::App(Box::new(l), Box::new(r)))
+            .debug("app");
 
         let abs = lambda
-                .ignore_then(ident)
-                .then(ty_annot)
-                .then_ignore(dot)
-                .then(term)
-                .map(|((var, arg_ty), body)| Term::Abs(var, Box::new(arg_ty), Box::new(body)));
+            .ignore_then(ident)
+            .then(ty_annot)
+            .then_ignore(dot)
+            .then(term)
+            .map(|((var, arg_ty), body)| Term::Abs(var, Box::new(arg_ty), Box::new(body)))
+            .debug("abs");
 
-        choice((abs, app, smallest)).then_ignore(end())
+        // Priority
+        // Abs > App > Lit > Ident > "("Term")"
+        choice((abs, app, smallest))
+            .then_ignore(end())
+            .debug("term")
+        //smallest.then_ignore(end())
     })
 }
 
@@ -133,7 +145,7 @@ impl Typing for Term {
             }
             Term::Var(var) => match ctx.get_type(var) {
                 Some(ty) => Ok(ty),
-                None => Err(format!("Couldn't determine type of `{}`", var.sym)),
+                None => Err(format!("Couldn't find type of `{}`", var.sym)),
             },
             Term::App(l, r) => {
                 let l_ty = l.typing(ctx)?;
@@ -182,3 +194,31 @@ fn main() {
     };
     println!("term: {:?}", ty);
 }
+
+#[cfg(test)]
+fn debug_parse(src: &str) -> (Option<Term>, Vec<Simple<char>>) {
+    let parser = parser();
+    parser.parse_recovery_verbose(src)
+}
+
+#[test]
+fn parse_ok() {
+    let (term, _) = debug_parse("unit");
+    assert_eq!(term, Some(Term::Lit(Lit::Unit)));
+
+    let (term, _) = debug_parse("lambda x: Unit. x");
+    assert_eq!(
+        term,
+        Some(Term::Abs(
+            Var::new("x".to_string()),
+            Box::new(Type::Unit),
+            Box::new(Term::Var(Var::new("x".to_string())))
+        ))
+    );
+
+    let (term, _) = debug_parse("( unit )");
+    assert_eq!(term, Some(Term::Lit(Lit::Unit)));
+}
+
+#[test]
+fn parse_err() {}
